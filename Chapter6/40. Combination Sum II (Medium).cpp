5089e59b614180b38922039383254b3dcc04f118
Talantolinyi <37531224+TalantoLinyi@users.noreply.github.com>
2022-11-14 12:05:51 +0800

    Create 40. Combination Sum II (Medium).cpp

---
+++ Chapter6/40. Combination Sum II (Medium).cpp
@@ -0,0 +1,24 @@
+class Solution {
+    vector<int> path;
+    vector<vector<int>> ans;
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        backtrace(candidates, target, 0);
+        return ans;
+    }
+
+    void backtrace(vector<int>& candidates, int target, int pos) {
+        if (target == 0) {
+            ans.push_back(path);
+            return;
+        }
+        if (pos == candidates.size()) return;
+        for (int i = pos; i < candidates.size() && candidates[i] <= target; ++i) {
+            if (i > pos && candidates[i] == candidates[i - 1]) continue;
+            path.push_back(candidates[i]);
+            backtrace(candidates, target - candidates[i], i + 1);
+            path.pop_back();
+        }
+    }
+};
