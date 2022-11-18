033055259484423b888957f60da230c6559936cd
Talantolinyi <37531224+TalantoLinyi@users.noreply.github.com>
2022-11-18 10:40:01 +0800

    Create 94. Binary Tree Inorder Traversal (Medium).cpp

---
+++ Chapter14/94. Binary Tree Inorder Traversal (Medium).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (!root) return ans;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while (node || !st.empty()) {
+            while (node) {
+                st.push(node);
+                node = node->left;
+            }
+            node = st.top(); st.pop();
+            ans.push_back(node->val);
+            node = node->right;
+        }
+        return ans;
+    }
+};
